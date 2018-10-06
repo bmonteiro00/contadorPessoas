ec90c5780ca1570e83434623629941e547bce459
bmonteiro00 <bmonteiro00@gmail.com>
2018-10-05 23:49:19 -0300

    adiciona gnss processador

--- src/gps/Gps.py
+++ src/gps/Gps.py
@@ -1,43 +1,39 @@
-import gps, os, time
+import socket, json
 
 
 class Gps:
 
     def __init__(self):
-        self.gps = gps.gps()
-        print('Satellites (total of', len(session.satellites), ' in view)')
-        for i in session.satellites:
-            print('\t', i)
+        self.gps_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.gps_server.connect(("localhost", "2947"))
+        self.gps_server.send('?WATCH={"enable": true,"json":true};')
+        self.gps.latitude = None
+        self.gps.longitude = None
+        self.gps.velocidade = None
 
     def getlatitude(self):
-        return self.gps.fix.latitude
+        return self.gps.latitude
 
     def getlongitude(self):
-        return self.gps.fix.longitude
-
-    def getaltitude(self):
-        return self.gps.fix.altitude
+        return self.gps.longitude
 
     def getvelocidade(self):
         return self.gps.speed
 
-    def printparametros(self):
-        while 1:
-            # a = altitude, d = date/time, m=mode,
-            # o=postion/fix, s=status, y=satellites
+    def capturaparametros(self):
 
-            print
-            print(' GPS reading')
-            print('----------------------------------------')
-            print('latitude    ', self.gps.fix.latitude)
-            print ('longitude   ', self.gps.fix.longitude)
-            print ('time utc    ', self.gps.utc, session.fix.time)
-            print ('altitude    ', self.gps.fix.altitude)
-            #print 'eph         ', self.gps.fix.eph
-            print ('epv         ', self.gps.fix.epv)
-            print ('ept         ', self.gps.fix.ept)
-            print ('speed       ', self.gps.fix.speed)
-            print ('climb       ', self.gps.fix.climb)
+        while 1:
+            json_response = self.gps_server.recv(2048).strip()
+            if json_response.find('"class":"TPV"') != -1:
+                json_list = json_response.split(',')
+                for pos in json_list:
+                    if pos.find("lat") != -1:
+                        self.gps.latitude = pos
+                    if pos.find("lon") != -1:
+                        self.gps.longitude = pos
+                    if pos.find("speed") != -1:
+                        self.gps.velocidade = pos
+
 
         time.sleep(3)
 
